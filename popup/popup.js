aa4fdfe72f0dcf4f56cf7eb6209718d6553057f7
Hyrum Seth White <hyrum.s.white@gmail.com>
2018-07-22 13:05:13 -0600

    popup no longer requires a2c active tab

--- popup/popup.js
+++ popup/popup.js
@@ -16,22 +16,22 @@ const sendMessageToActiveTab = message => {
     );
   });
 };
-const getDMETab = () => {
+const getTabByUrl = url => {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({ currentWindow: true }, tabs => {
       let dmeTab;
       for (let tab of tabs) {
-        if (tab.url.includes("dmelive.com/Trips/")) {
+        if (tab.url.includes(url)) {
           dmeTab = tab;
           return resolve(dmeTab);
         }
       }
-      reject({ error: "no dme tab found" });
+      reject({ error: "no tab found with url: " + url });
     });
   });
 };
-const sendMessageToDMETab = async message => {
-  let dmeTab = await getDMETab();
+const sendMessageToTabByURL = async (url, message) => {
+  let dmeTab = await getTabByUrl(url);
   return new Promise((resolve, reject) => {
     sendMessage(dmeTab, message).then(resolve);
   });
@@ -39,14 +39,42 @@ const sendMessageToDMETab = async message => {
 
 const simpleCopy = async $event => {
   try {
-    let pageData = await sendMessageToActiveTab({ action: "sendDataToDME" });
+    let pageData = await sendMessageToTabByURL(
+      "https://a2ctp.emsc.net/TripDetails.aspx",
+      { action: "sendDataToDME" }
+    );
     pageData.action = "fillDataFromA2C";
-    let fillResults = await sendMessageToDMETab(pageData);
+    let fillResults = await sendMessageToTabByURL(
+      "dmelive.com/Trips/",
+      pageData
+    );
   } catch (e) {
     throw new Error(e);
   }
 };
 
-document
-  .getElementById("start-transfer-button")
-  .addEventListener("click", simpleCopy);
+const startButton = document.getElementById("start-transfer-button");
+startButton.addEventListener("click", simpleCopy);
+const a2cStatusElement = document.getElementById("a2c-page-status");
+const dmeStatusElement = document.getElementById("dme-page-status");
+getTabByUrl("TripDetails.aspx")
+  .then(tab => {
+    a2cStatusElement.textContent = "Found A2C trip details page";
+    a2cStatusElement.style.color = "rgb(76, 175, 80)";
+  })
+  .catch(error => {
+    a2cStatusElement.textContent = "A2C trip details page not found";
+    a2cStatusElement.style.color = "rgb(244, 67, 54)";
+    startButton.setAttribute("disabled", "true");
+  });
+
+getTabByUrl("http://dmelive.com/Trips/mgmttrips.aspx")
+  .then(tab => {
+    dmeStatusElement.textContent = "Found DME trip entry page";
+    dmeStatusElement.style.color = "rgb(76, 175, 80)";
+  })
+  .catch(error => {
+    dmeStatusElement.textContent = "DME trip entry page not found";
+    dmeStatusElement.style.color = "rgb(244, 67, 54)";
+    startButton.setAttribute("disabled", "true");
+  });
